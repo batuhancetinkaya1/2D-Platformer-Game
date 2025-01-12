e1137a8543cb130957f7f9c953ca05cc3de49c58
mercanuz <84629250+mercanuz@users.noreply.github.com>
2025-01-12 16:39:14 +0300

    Update ArenaManager.cs
    Player vs bot

--- 2D Platform Game/Assets/Scripts/Game&CameraScripts/ArenaManager.cs
+++ 2D Platform Game/Assets/Scripts/Game&CameraScripts/ArenaManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class ArenaManager : MonoBehaviour
 {
-    [SerializeField] private Transform[] m_spawnPoints; // Sahnedeki spawn noktalar˝
+    [SerializeField] private Transform[] m_spawnPoints; // Sahnedeki spawn noktalar√Ω
     [SerializeField] private GameObject m_playerPrefab;
     [SerializeField] private GameObject m_player2Prefab;
     [SerializeField] private GameObject m_botPrefab;
@@ -12,7 +12,7 @@ public class ArenaManager : MonoBehaviour
 
     private void Start()
     {
-        string mode = PlayerPrefs.GetString("ArenaMode", "PVP");
+        string mode = PlayerPrefs.GetString("ArenaMode", "PVB");
         SetupArena(mode);
     }
 
@@ -21,7 +21,7 @@ public class ArenaManager : MonoBehaviour
         switch (mode)
         {
             case "PVP":
-                // ÷rnein 2 player instantiate et
+                // √ñrne√∞in 2 player instantiate et
                 GameObject player1 = Instantiate(m_playerPrefab, m_spawnPoints[0].position, Quaternion.identity);
                 GameObject player2 = Instantiate(m_player2Prefab, m_spawnPoints[1].position, Quaternion.identity);
                 break;
@@ -33,7 +33,7 @@ public class ArenaManager : MonoBehaviour
                 break;
 
             case "AIvsAI":
-                // ›ki bot
+                // √ùki bot
                 GameObject enemy2 = Instantiate(m_bot2Prefab, m_spawnPoints[0].position, Quaternion.identity);
                 GameObject enemy3 = Instantiate(m_botPrefab, m_spawnPoints[1].position, Quaternion.identity);
                 break;
